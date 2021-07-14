0b2bd86fce1dc01dbb7912712b37fdbac39b48f0
jbreazy11 <bowen.j11@gmail.com>
2021-07-14 21:33:26 +1000

    break and study timer run and count number of work timers that have ran. completed first dev log. working on function so timers loop.

--- development-log.md
+++ development-log.md
@@ -1,7 +1,9 @@
 # Development Log
 
 14/07/21
-Progress for the Pomodoro style x Tamagotchi app is well underway. A trello board has been set up with each task that needs to be completed, organised by colour from most important to least important with some bonus goals listed if the other work is completed. 
+Progress for the Pomodoro style x Tamagotchi app is well underway. A trello board has been set up with each task that needs to be completed, organised by colour from most important to least important with some bonus goals listed if the other work is completed. A 25 minute timer and 5 minute break timer have been coded, with a 15 minute timer that runs after four 25 minutes timers. These timers only begin after input from the user. 
+ 
+Next on the agenda is to incorporate a timer that runs in the background in between the work and break timers that will either add or remove points from a counter. The counter will be used to keep track of the happiness of the pet avatar, going up when the break or study timer is running, and going down when either of those timers are not running. A flowchart is being used to keep track of the flow of the application. 
 
 
 Done: 
--- main.py
+++ main.py
@@ -1,23 +1,47 @@
 import time
-import datetime
-import sys
 
-print("Welcome to your best and newest Pomodoro app!")
-input("Press enter to begin 25 minute work timer: ")
-
+# Keeps track of number of study times
 study_times = 0
-
+# 25 minute timer
+# dont forget to change run time
 def work_timer():
     global study_times
     minutes = 0
     print("25 minute timer starts now!")
-    while minutes <= 25:
+    while minutes <= 1:
         time.sleep(60)
         minutes = minutes + 1
+        study_times =+ 1
     print(f"{study_times} study times completed... Time for a break!")
 
-work_timer()
-
+# 5 minute timer
+# dont forget to change run time
 def break_timer():
     global study_times
-    
+    minutes = 0 
+    if study_times <4:
+        print("5 minute break! Let's go!")
+        while minutes != 1:
+            time.sleep(60)
+            minutes = minutes + 1
+            print("break over :( ")
+    elif study_times == 4:
+        print("You've earned a long break.")
+        while minutes != 1:
+            time.sleep(60)
+            minutes = minutes + 1
+            print("long break over :( ")
+        study_times = 0
+
+
+welcome = print("Welcome to your best and newest Pomodoro app!")
+work_start = input("Press enter to begin 25 minute work timer: ")
+break_start = input("Press enter to start break timer")
+print(study_times)
+
+def pomodoro():
+    print(welcome)
+    print(work_start)
+    work_timer()
+    print(break_start)
+    break_timer()
